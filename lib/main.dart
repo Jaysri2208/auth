4ad76ba781a59bad34b883afb365db520b8a7691
Jaysri2208 <jayaa@student.tce.edu>
2024-05-20 00:05:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyDCM_pGg-_TCcxD4Lt5FC794sKcjFrjgkU',
+    appId: '1:909600456266:web:ca67cbc7ee4c0ec6bdf0b3',
+    messagingSenderId: '909600456266',
+    projectId: 'foood-82d9b',
+    authDomain: 'foood-82d9b.firebaseapp.com',
+    storageBucket: 'foood-82d9b.appspot.com',
+    measurementId: 'G-E83SLLWJ5J',
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
